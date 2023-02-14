308e81828d61bd2cd7811bb73c081c187942b7ad
prakharsinghal311 <prakhargd31121999@gmail.com>
2023-02-15 00:28:44 +0530

    Expense Items

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,13 @@
+import React from "react";
 function ExpenseItem() {
-  return;
-  <h2>Expense Items</h2>;
+  return (
+    <div>
+      <div>Expense Items!</div>
+      <div>Food Rs 10</div>
+      <div>Petrol Rs 100</div>
+      <div>Movies Rs 200</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
