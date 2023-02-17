68abb7ea789aaaa6b71a3bb4c918fa3f40793f56
prakharsinghal311 <prakhargd31121999@gmail.com>
2023-02-17 12:58:54 +0530

    Passing data way "props" and adding css styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,16 @@
 import React from "react";
-function ExpenseItem() {
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
   return (
     <div>
-      <div>Expense Items!</div>
-      <div>Food Rs 10</div>
-      <div>Petrol Rs 100</div>
-      <div>Movies Rs 200</div>
+      <div className="expense-item">
+        <div>{props.date.toISOString()}</div>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <h2>{props.location}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </div>
     </div>
   );
 }
